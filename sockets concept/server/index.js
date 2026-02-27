b8b02855541850351a28491bf17442cd4b5b4ab0
aishanisingh2003 <aishani18122003@gmail.com>
2026-02-27 17:06:02 +0530

    socket concept

---
+++ sockets concept/server/index.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import {Server} from 'socket.io'
+import http from 'http'
+import path from 'path'
+import { Socket } from 'dgram';
+
+const PORT=3000;
+const app=express();
+// now we are creating http server
+const server=http.createServer(app);
+app.use(express.static(path.resolve("./public")));
+const io=new Server(server);
+  
+//socket io handling
+io.on("connection",(socket)=>{
+  socket.on('user-message',message=>{
+    io.emit('message',message);
+  });
+});
+
+app.get('/',(req,res)=>{
+  return res.sendFile("/public/index.html");
+})
+
+
+
+server.listen(PORT,()=>{
+  console.log(`server is running on port ${PORT}`);
+})
